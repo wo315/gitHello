4e2b7bae5db2b7c54ff09e27806eb2c21f5e10a9
wo315 <2106936@qq.com>
2022-07-01 16:27:00 +0800

    first github repository

---
+++ gitHello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    std::vector<std::string> hello = {"Hello", "World", "OpenFOAM", "and", "git"};
+
+    for(string& s : hello) {
+        std::cout << s << " ";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+
+}
